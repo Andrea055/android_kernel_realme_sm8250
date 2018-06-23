7692e895b9fc6b6089f1d52926217ec1039ad963
flar2 <asegaert@gmail.com>
2018-06-23 00:26:34 -0400

    drm: msm: kcal: hue, saturation, value and contrast adjustments
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- techpack/display/msm/sde/sde_hw_color_proc_v4.c
+++ techpack/display/msm/sde/sde_hw_color_proc_v4.c
@@ -8,29 +8,21 @@
 #include "sde_hw_color_proc_common_v4.h"
 #include "sde_hw_color_proc_v4.h"
 
-#ifdef CONFIG_KLAPSE
-#include <linux/klapse.h>
+unsigned int kcal_red = 256;
+unsigned int kcal_green = 256;
+unsigned int kcal_blue = 256;
+unsigned int kcal_hue = 0;
+unsigned int kcal_sat = 255;
+unsigned int kcal_val = 255;
+unsigned int kcal_cont = 255;
 
-unsigned short kcal_red = 256;
-unsigned short kcal_green = 256;
-unsigned short kcal_blue = 256;
-#else
-static unsigned short kcal_red = 256;
-static unsigned short kcal_green = 256;
-static unsigned short kcal_blue = 256;
-#endif
-static unsigned short kcal_hue = 0;
-static unsigned short kcal_sat = 255;
-static unsigned short kcal_val = 255;
-static unsigned short kcal_cont = 255;
-
-module_param(kcal_red, short, 0644);
-module_param(kcal_green, short, 0644);
-module_param(kcal_blue, short, 0644);
-module_param(kcal_hue, short, 0644);
-module_param(kcal_sat, short, 0644);
-module_param(kcal_val, short, 0644);
-module_param(kcal_cont, short, 0644);
+module_param(kcal_red, uint, 0644);
+module_param(kcal_green, uint, 0644);
+module_param(kcal_blue, uint, 0644);
+module_param(kcal_hue, uint, 0644);
+module_param(kcal_sat, uint, 0644);
+module_param(kcal_val, uint, 0644);
+module_param(kcal_cont, uint, 0644);
 
 static int sde_write_3d_gamut(struct sde_hw_blk_reg_map *hw,
 		struct drm_msm_3d_gamut *payload, u32 base,
@@ -294,6 +286,7 @@ void sde_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 		}
 
 		SDE_REG_WRITE(&ctx->hw, base + PCC_C_OFF, coeffs->c);
+
 		// RED
 		SDE_REG_WRITE(&ctx->hw, base + PCC_R_OFF,
 			i == 0 ? (coeffs->r * kcal_red) / 256 : coeffs->r);
@@ -303,11 +296,13 @@ void sde_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 		// BLUE
 		SDE_REG_WRITE(&ctx->hw, base + PCC_B_OFF,
 			i == 2 ? (coeffs->b * kcal_blue) / 256 : coeffs->b);
+
 		SDE_REG_WRITE(&ctx->hw, base + PCC_RG_OFF, coeffs->rg);
 		SDE_REG_WRITE(&ctx->hw, base + PCC_RB_OFF, coeffs->rb);
 		SDE_REG_WRITE(&ctx->hw, base + PCC_GB_OFF, coeffs->gb);
 		SDE_REG_WRITE(&ctx->hw, base + PCC_RGB_OFF, coeffs->rgb);
 	}
+
 	opcode = SDE_REG_READ(&ctx->hw, ctx->cap->sblk->hsic.base);
 
 	// HUE
@@ -329,6 +324,9 @@ void sde_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 	SDE_REG_WRITE(&ctx->hw, ctx->cap->sblk->hsic.base + PA_CONT_OFF,
 		kcal_cont & PA_CONT_MASK);
 	local_opcode |= PA_CONT_EN;
+
+	opcode |= (local_opcode | PA_EN);
+	SDE_REG_WRITE(&ctx->hw, ctx->cap->sblk->hsic.base, opcode);
 
 	opcode |= (local_opcode | PA_EN);
 	SDE_REG_WRITE(&ctx->hw, ctx->cap->sblk->hsic.base, opcode);
