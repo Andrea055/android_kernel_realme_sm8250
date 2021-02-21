0ba433f11acbd1e49723f147af8858a6b5fc046c
LibXZR <xzr467706992@163.com>
2021-02-21 15:34:35 +0800

    msm_performance: Block userspace from changing min / max frequency
    * Userspace is doing nothing great
    
    Change-Id: I487f3bf49a838ea55236adf83be0d26d9d78cde6
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -83,8 +83,8 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 
-	if (touchboost == 0)
-		return 0;
+	return 0;
+
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
@@ -164,9 +164,8 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 
-	if (touchboost == 0)
-		return 0;
-		
+	return 0;
+
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
