c96ff9290d7ed54dd4af4dccc31b706747a568a3
Andrea Canale <vivanco11.ac@gmail.com>
2023-02-14 21:14:05 +0100

    qcacld-3.0: Reduce logging

--- drivers/staging/qcacld-3.0/core/wma/src/wma_features.c
+++ drivers/staging/qcacld-3.0/core/wma/src/wma_features.c
@@ -2416,6 +2416,7 @@ wma_wake_reason_ap_assoc_lost(t_wma_handle *wma, void *event, uint32_t len)
 	return 0;
 }
 
+#if 0
 static const char *wma_vdev_type_str(uint32_t vdev_type)
 {
 	switch (vdev_type) {
@@ -2437,6 +2438,7 @@ static const char *wma_vdev_type_str(uint32_t vdev_type)
 		return "unknown";
 	}
 }
+#endif
 
 static int wma_wake_event_packet(
 	t_wma_handle *wma,
